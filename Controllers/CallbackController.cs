b9672645cb3111cf46c09e3c7734fa8493921bdb
Muhamadjoni Rahimzod <mrahimzod@gmail.com>
2022-04-07 16:05:56 +0400

    debugging the call bot

--- Controllers/CallbackController.cs
+++ Controllers/CallbackController.cs
@@ -1,28 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using CallingBotSample.Bots;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.Integration.AspNet.Core;
 
 namespace CallingMeetingBot.Controllers
 {
-    [Route("callback")]
-    public class CallbackController: Controller
+  [Route("callback")]
+  public class CallbackController : Controller
+  {
+    private readonly CallingBot bot;
+
+    public CallbackController(CallingBot bot)
     {
-        private readonly CallingBot bot;
-
-        public CallbackController(CallingBot bot)
-        {
-            this.bot = bot;
-        }
-
-        [HttpPost, HttpGet]
-        public async Task HandleCallbackRequestAsync()
-        {
-            await this.bot.ProcessNotificationAsync(this.Request, this.Response).ConfigureAwait(false);
-        }
+      this.bot = bot;
     }
+
+    [HttpPost, HttpGet]
+    public async Task HandleCallbackRequestAsync()
+    {
+      await this.bot.ProcessNotificationAsync(this.Request, this.Response).ConfigureAwait(false);
+    }
+  }
 }
